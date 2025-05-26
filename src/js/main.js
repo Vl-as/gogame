f97d37eb0ec059b9b43aa74a854c95697adf2dd7
Vladimir <vladimiraseev89@gmail.com>
2025-05-27 01:14:05 +0700

    add responsive header and create burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import "/src/sass/style.scss";
+
+
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
